bf1a4480cc02fc2ea9857ccf8f7937d1ef65fd47
GUSH <k115601@naver.com>
2024-07-02 11:47:54 +0900

    태스크 폼 css 추가

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./TaskForm.css";
 
 export default function TaskForm() {
   return (
@@ -11,19 +12,22 @@ export default function TaskForm() {
         />
 
         <div className="task_form_bottom_line">
-          <button className="tag">HTML</button>
-          <button className="tag">CSS</button>
-          <button className="tag">JavaScript</button>
-          <button className="tag">React</button>
-
-          <select className="task_status">
-            <option value="todo">할일</option>
-            <option value="todo">진행중</option>
-            <option value="todo">완료</option>
-          </select>
-          <button type="submit" className="task_submit">
-            + 추가
-          </button>
+          <div>
+            <button className="tag">HTML</button>
+            <button className="tag">CSS</button>
+            <button className="tag">JavaScript</button>
+            <button className="tag">React</button>
+          </div>
+          <div>
+            <select className="task_status">
+              <option value="todo">할일</option>
+              <option value="todo">진행중</option>
+              <option value="todo">완료</option>
+            </select>
+            <button type="submit" className="task_submit">
+              + 추가
+            </button>
+          </div>
         </div>
       </form>
     </header>

4bc2afd803c85d26f0f8a97b680b4f77e8b9ee3a
GUSH <k115601@naver.com>
2024-07-02 10:32:46 +0900

    생성 및 초기설정

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app_header">Header Section</header>
+      <main className="app_main">
+        <section className="task_column">section 1</section>
+        <section className="task_column">section 2</section>
+        <section className="task_column">section 3</section>
+      </main>
+    </div>
+  );
+}
+
+export default App;

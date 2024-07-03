a58d2d1bad996d579df8f74630b32eed70156a50
GUSH <k115601@naver.com>
2024-07-03 16:03:06 +0900

    form 버튼 handleSubmit 추가

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 export default function TaskForm() {
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+  });
+  const [task, setTask] = useState("");
+  const [status, setStatus] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  };
   return (
     <header className="app_header">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
+          onChange={handleChange}
+          value={taskData.task}
+          name="task"
           className="task_input"
           placeholder="Enter your task"
         />
@@ -20,10 +40,15 @@ export default function TaskForm() {
             <Tag tagName="REACT" />
           </div>
           <div>
-            <select className="task_status">
+            <select
+              className="task_status"
+              name="status"
+              value={taskData.status}
+              onChange={handleChange}
+            >
               <option value="todo">할일</option>
-              <option value="todo">진행중</option>
-              <option value="todo">완료</option>
+              <option value="doing">진행중</option>
+              <option value="done">완료</option>
             </select>
             <button type="submit" className="task_submit">
               + 추가
